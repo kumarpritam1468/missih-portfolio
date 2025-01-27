85da8b40d5cf795abaf2c04406a4e868fb393017
Pritam <manoharipritam@gmail.com>
2025-01-27 20:04:10 +0530

    change graph style

---
+++ src/lib/graphConfig.ts
@@ -0,0 +1,88 @@
+import { Edge, Node } from "@xyflow/react";
+
+export const initialNodes: Node[] = [
+    // Main concepts with draggable set to false
+    {
+        id: '1',
+        position: { x: 400, y: 30 },
+        data: { label: 'Machine Learning' },
+        className: 'custom-node',
+        draggable: false
+    },
+    {
+        id: '2',
+        position: { x: 100, y: 180 },
+        data: { label: 'Data Science' },
+        className: 'custom-node',
+        draggable: false
+    },
+    {
+        id: '3',
+        position: { x: 780, y: 180 },
+        data: { label: 'AI Ethics' },
+        className: 'custom-node',
+        draggable: false
+    },
+    // Sub concepts
+    {
+        id: '4',
+        position: { x: 300, y: 150 },
+        data: { label: 'Neural Networks' },
+        className: 'custom-node',
+        draggable: false
+    },
+    {
+        id: '5',
+        position: { x: 500, y: 150 },
+        data: { label: 'Deep Learning' },
+        className: 'custom-node',
+        draggable: false
+    },
+    {
+        id: '6',
+        position: { x: 0, y: 300 },
+        data: { label: 'Big Data' },
+        className: 'custom-node',
+        draggable: false
+    },
+    {
+        id: '7',
+        position: { x: 200, y: 300 },
+        data: { label: 'Statistics' },
+        className: 'custom-node',
+        draggable: false
+    },
+    {
+        id: '8',
+        position: { x: 600, y: 300 },
+        data: { label: 'Bias' },
+        className: 'custom-node',
+        draggable: false
+    },
+    {
+        id: '9',
+        position: { x: 800, y: 300 },
+        data: { label: 'Privacy' },
+        className: 'custom-node',
+        draggable: false
+    },
+    {
+        id: '10',
+        position: { x: 1000, y: 300 },
+        data: { label: 'Fairness' },
+        className: 'custom-node',
+        draggable: false
+    },
+];
+
+const initialEdgeStyle = { stroke: 'rgb(89, 238, 95)', strokeWidth: 2.5, strokeDasharray: 8, animation: 'dash 10s linear infinite' };
+
+export const initialEdges: Edge[] = [
+    { id: 'e1-4', source: '1', target: '4', style: initialEdgeStyle },
+    { id: 'e1-5', source: '1', target: '5', style: initialEdgeStyle },
+    { id: 'e2-6', source: '2', target: '6', style: initialEdgeStyle },
+    { id: 'e2-7', source: '2', target: '7', style: initialEdgeStyle },
+    { id: 'e3-8', source: '3', target: '8', style: initialEdgeStyle },
+    { id: 'e3-9', source: '3', target: '9', style: initialEdgeStyle },
+    { id: 'e3-10', source: '3', target: '10', style: initialEdgeStyle },
+];
--- src/pages/RabbitHole.tsx
+++ src/pages/RabbitHole.tsx
@@ -4,10 +4,9 @@ import {
   Controls,
   useNodesState,
   useEdgesState,
-  addEdge,
+  // addEdge,
   Node,
-  Edge,
-  Connection,
+  // Connection,
   NodeMouseHandler
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
@@ -25,106 +24,23 @@ import { contentData } from "../data/data"
 import { ArrowLeft, ArrowRight } from 'lucide-react';
 import { useSwipeable } from 'react-swipeable';
 import { motion, AnimatePresence } from "framer-motion";
+import {initialNodes, initialEdges} from "@/lib/graphConfig.ts";
 
-const initialNodes: Node[] = [
-  // Main concepts with draggable set to false
-  {
-    id: '1',
-    position: { x: 400, y: 30 },
-    data: { label: 'Machine Learning' },
-    className: 'custom-node',
-    draggable: false
-  },
-  {
-    id: '2',
-    position: { x: 100, y: 180 },
-    data: { label: 'Data Science' },
-    className: 'custom-node',
-    draggable: false
-  },
-  {
-    id: '3',
-    position: { x: 780, y: 180 },
-    data: { label: 'AI Ethics' },
-    className: 'custom-node',
-    draggable: false
-  },
-  // Sub concepts
-  {
-    id: '4',
-    position: { x: 300, y: 150 },
-    data: { label: 'Neural Networks' },
-    className: 'custom-node',
-    draggable: false
-  },
-  {
-    id: '5',
-    position: { x: 500, y: 150 },
-    data: { label: 'Deep Learning' },
-    className: 'custom-node',
-    draggable: false
-  },
-  {
-    id: '6',
-    position: { x: 0, y: 300 },
-    data: { label: 'Big Data' },
-    className: 'custom-node',
-    draggable: false
-  },
-  {
-    id: '7',
-    position: { x: 200, y: 300 },
-    data: { label: 'Statistics' },
-    className: 'custom-node',
-    draggable: false
-  },
-  {
-    id: '8',
-    position: { x: 600, y: 300 },
-    data: { label: 'Bias' },
-    className: 'custom-node',
-    draggable: false
-  },
-  {
-    id: '9',
-    position: { x: 800, y: 300 },
-    data: { label: 'Privacy' },
-    className: 'custom-node',
-    draggable: false
-  },
-  {
-    id: '10',
-    position: { x: 1000, y: 300 },
-    data: { label: 'Fairness' },
-    className: 'custom-node',
-    draggable: false
-  },
-];
-
-const initialEdges: Edge[] = [
-  { id: 'e1-4', source: '1', target: '4', style: { stroke: 'rgb(204, 255, 204)' } },
-  { id: 'e1-5', source: '1', target: '5', style: { stroke: 'rgb(204, 255, 204)' } },
-  { id: 'e2-6', source: '2', target: '6', style: { stroke: 'rgb(204, 255, 204)' } },
-  { id: 'e2-7', source: '2', target: '7', style: { stroke: 'rgb(204, 255, 204)' } },
-  { id: 'e3-8', source: '3', target: '8', style: { stroke: 'rgb(204, 255, 204)' } },
-  { id: 'e3-9', source: '3', target: '9', style: { stroke: 'rgb(204, 255, 204)' } },
-  { id: 'e3-10', source: '3', target: '10', style: { stroke: 'rgb(204, 255, 204)' } },
-];
 
 export default function RabbitHole(): JSX.Element {
   const [nodes, _setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [edges, _setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const [selectedNode, setSelectedNode] = useState<Node | null>(null);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [direction, setDirection] = useState<"left" | "right">("left");
 
-  const onConnect = useCallback(
-    (params: Connection) =>
-      setEdges((eds) =>
-        addEdge({ ...params, style: { stroke: "rgb(204, 255, 204)" } }, eds)
-      ),
-    [setEdges]
-  );
+  // const onConnect = useCallback(
+  //   (params: Connection) =>
+  //     setEdges((eds) =>
+  //       addEdge({ ...params, style: { stroke: "rgb(204, 255, 204)" } }, eds)
+  //     ),
+  //   [setEdges]
+  // );
 
   const onNodeClick: NodeMouseHandler = useCallback((_event: any, node: Node) => {
     setSelectedNode(node);
@@ -173,16 +89,18 @@ export default function RabbitHole(): JSX.Element {
     <div className="h-screen w-screen relative">
       <style>
         {`
-          .custom-node {
-            padding: 10px;
-            border-radius: 5px;
-            color: rgb(204, 255, 204);
-            cursor: pointer;
-          }
           .react-flow__node-default {
             background: black;
             border: 2px solid rgb(89, 238, 95);
-            box-shadow: 0 0 1rem #22c55e;
+            box-shadow: 0 0 3rem rgba(34, 197, 94, .6);
+            font-size: .8rem;
+            padding: 10px;
+            border-radius: 1rem;
+            color: rgb(204, 255, 204);
+            min-width: 150px;
+            width: fit-content;
+            cursor: pointer;
+            transition: all .2s ease-in-out;
           }
         `}
       </style>
@@ -191,7 +109,7 @@ export default function RabbitHole(): JSX.Element {
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
+        // onConnect={onConnect}
         onNodeClick={onNodeClick}
         fitView
       >
